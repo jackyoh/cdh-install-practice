9a6690ab2bff4bc3d74e464447e093faece9bc86
jack <jack@is-land.com.tw>
2017-12-20 16:43:08 +0800

    Update cdh version

--- cdh-repository-server/download-rpm.sh
+++ cdh-repository-server/download-rpm.sh
@@ -2,11 +2,11 @@
 outputPath=/var/www/html/cloudera
 
 mkdir -p $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.7.0/RPMS/x86_64/cloudera-manager-agent-5.7.0-1.cm570.p0.76.el7.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.7.0/RPMS/x86_64/cloudera-manager-daemons-5.7.0-1.cm570.p0.76.el7.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.7.0/RPMS/x86_64/cloudera-manager-server-5.7.0-1.cm570.p0.76.el7.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.7.0/RPMS/x86_64/cloudera-manager-server-db-2-5.7.0-1.cm570.p0.76.el7.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.7.0/RPMS/x86_64/enterprise-debuginfo-5.7.0-1.cm570.p0.76.el7.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/oracle-j2sdk1.7-1.7.0+update67-1.x86_64.rpm -P $outputPath
-wget https://archive.cloudera.com/cdh5/sles/12/x86_64/cdh/RPM-GPG-KEY-cloudera -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/cloudera-manager-agent-5.13.1-1.cm5131.p0.2.el7.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/cloudera-manager-daemons-5.13.1-1.cm5131.p0.2.el7.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/cloudera-manager-server-5.13.1-1.cm5131.p0.2.el7.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/cloudera-manager-server-db-2-5.13.1-1.cm5131.p0.2.el7.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/enterprise-debuginfo-5.13.1-1.cm5131.p0.2.el7.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/oracle-j2sdk1.7-1.7.0+update67-1.x86_64.rpm -P $outputPath
+wget http://archive.cloudera.com/cm5/redhat/7/x86_64/cm/5.13/RPMS/x86_64/jdk-6u31-linux-amd64.rpm -P $outputPath
 createrepo $outputPath
